9f03187cec7bbe7c5609fd546dde42bb79844426
Sagar <dev.sagartmg@gmail.com>
2023-04-17 16:50:53 +0545

    git info

---
+++ git.js
@@ -0,0 +1,18 @@
+/* setup
+    1. git init
+    2. git remote add origin <git_url> 
+*/
+
+/* update the code 
+    1. git status 
+        to check the changes in code
+    2. add the code 
+        - git add . // git add all  
+        or
+        - git add <file_path>
+    3. git commit -m "feature_name"
+    4. git push
+
+
+
+*/
