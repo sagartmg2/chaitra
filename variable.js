f16669988291c7f058884c19b31c85068f42d944
Sagar <dev.sagartmg@gmail.com>
2023-04-26 17:09:21 +0545

    variable

---
+++ variable.js
@@ -0,0 +1,99 @@
+
+console.log("Hello World!")
+console.log("Web Development")
+
+// line-comment  - ctrl+/
+
+/* 
+
+    block commnet - alt+shift+a
+
+    print("hello world")
+    System.out.println("HelloWor ") ;
+    echo()
+
+*/
+
+
+/* 
+    varaibles (box / container)
+        --primary-color: #red
+
+    - value assign 
+    - reuse
+*/
+web = "web dev "
+
+course = web
+age = 20
+price = 100.101
+active = false
+
+// console.log(Web)
+console.log("course: ", course)
+console.log("staus is ", active)
+console.log("age is", age)
+console.log("price is", price)
+
+
+user_name = "ram"  // snake_case
+brandName = "sony" // camelCase
+
+console.log("user name : ", user_name)
+console.log("brand name : ", brandName)
+
+
+/* DATA TYPES 
+    - String  // text
+    - number
+        integer - whole nmber
+        decimal/double/float -  decimal
+    boolean - true/false
+
+*/
+
+
+
+/* var vs let vs const  */
+
+var color = "red"
+let pixel = 1000
+const PI = 3.14
+
+
+/* initialization vs decleartion */
+
+var day  //  declration
+day = "Sunday"  // initialization
+console.log("day", day)
+
+
+let year; // declartion
+year = 2023
+console.log("year", year)
+
+const EARTH_ROTATION = "west-east"; // both decleartion and initialization
+console.log("earth rotation", EARTH_ROTATION)
+
+
+var time = "5:00PM"
+var time = "5:01PM"
+console.log("time", time);
+
+
+let month = "Chaitra"
+// let month = "Baisakh" // cannot re-declare
+month = "Baisaksh"
+month = "Jestha" // re-initialziation
+console.log("month is", month)
+
+// console.log(first)
+// console.log();
+
+
+
+
+
+
+
+
