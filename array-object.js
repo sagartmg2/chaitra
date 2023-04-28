53661540c283b89350121c545e89439a801d79a9
Sagar <dev.sagartmg@gmail.com>
2023-04-28 17:11:53 +0545

    array and objects

---
+++ array-object.js
@@ -0,0 +1,168 @@
+/* VARAIBLES */
+
+let color; //  decleration 
+color = "red" // initialization 
+color = "green" // re-initialization 
+
+/* Data types
+
+    String
+    Number
+    Boolean - true false
+    undefined - if a varaiable has no any value
+    null
+
+
+    Non-primitive data types - Collections
+        - array
+    
+
+
+*/
+
+const age = 20;
+// age = 30
+
+let brand;
+console.log("brand", brand)
+
+let price = null
+
+
+let color_1 = "red"
+let color_2 = "green"
+let color_3 = "black"
+
+
+/* array 
+    - collection of different vaues
+    - collection of similar data-types
+
+    synatax
+    - let <varaible_name> = [  <value>, <value>  ]
+    
+ */
+
+let colors = ["red", "white", "orange"]
+let statuses = [true, false, true]
+
+
+
+/* object 
+     synatax
+    - let <varaible_name> = {
+        <key or property or attribute > : <value>
+        <key> : <value>
+    }
+
+*/
+
+let room_5 = {
+    ceiling_type: "wooden",
+    color: "green",
+    projector: {
+        color: "white"
+    }
+}
+
+let room_4 = {
+    ceiling_type: "concrete",
+    color: "green"
+}
+
+let frontend = {
+    tools: ["git", "vs-code", "api-testing-tool"]
+}
+let backend = {
+    tools: ["database", "api"]
+}
+
+let web_stack = [frontend, backend]
+console.log(web_stack);
+let security = ["system", "docker"]
+
+
+
+
+
+let class_1 = [
+    (section = [{ name: "A" }])
+]
+
+let class_2 = [
+    [{ name: "A" }]
+]
+
+
+// console.log(class_1);
+
+let class_one = {
+    class_teacher: {
+        name: "xyz",
+        phone: "12312"
+    },
+    students: [
+        {
+            name: "ram",
+            address: "bagbazar"
+        },
+        {
+            name: "shyma",
+            name: "sanepa",
+        }
+    ]
+}
+
+
+
+// let class_one = [
+//     {
+//         section: "A",
+//         class_teacher: {
+//             name: "xyz",
+//             phone: "12312"
+//         },
+//         students: [
+//             {
+//                 name: "ram",
+//                 address: "bagbazar"
+//             },
+//             {
+//                 name: "shyma",
+//                 name: "sanepa",
+//             }
+//         ]
+//     },
+//     {
+//         section: "B",
+//         class_teacher: {
+//             name: "xyz",
+//             phone: "12312"
+//         },
+//         students: [
+//             {
+//                 name: "ram",
+//                 address: "bagbazar"
+//             },
+//             {
+//                 name: "shyma",
+//                 name: "sanepa",
+//             }
+//         ]
+//     },
+// ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
