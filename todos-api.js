6fc933896f225de32e7f4371819a5687abe1a180
Sagar <dev.sagartmg@gmail.com>
2023-05-16 18:20:08 +0545

    api testing

---
+++ todos-api.js
@@ -0,0 +1,35 @@
+/* Database 
+
+    CRUD operation
+        create
+        read
+        update 
+        delete
+    
+    HTTP request methods
+    GET - read / fetch 
+    POST  - create 
+    
+
+    HTTP status codes
+    2 ( success )
+        200
+        201 
+        203
+        204
+    3 ( redirect )
+        302 
+        304
+        307
+        308 
+    4 (  error -> Client Error )
+        400 - Bad request -> invalid form data
+        401 - not logged in 
+        403 - forbidden 
+        404 - resource not found
+    5
+        500 - Server Error
+
+
+
+*/
